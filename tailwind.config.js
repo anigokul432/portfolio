70a38fe90fcdf86944b1862c8b7222d7ff6a71f1
anigokul432 <anigokul432@gmail.com>
2022-08-25 18:46:20 -0700

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./public/index.html'],
   theme: {
     extend: {},
+    // fontFamily: {
+    //   'body': ['Helvetica', 'Arial', 'sans-serif'],
+    // }
   },
   plugins: [],
 }
